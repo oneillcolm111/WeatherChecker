f42915e95a183f45498e8c426968c7604ef96547
oneillcolm111 <oneill_colly@hotmail.com>
2024-07-04 09:43:30 +0100

    extra tests added

--- weather_api/tests/test_main.py
+++ weather_api/tests/test_main.py
@@ -9,27 +9,28 @@ app = FastAPI()
 client = TestClient(app)
 
 @pytest.fixture()
+
 def test_data():
     return {"city": "london", "date": "2024-06-01"}
 
-def test_create_weather_entry(test_data):
+def test_create_weather_entry2(test_data):
     response = client.post("/weather/", json=test_data)
     assert response.status_code == 201
     assert response.json()["city"] == test_data["city"]
     assert response.json()["date"] == test_data["date"]
 
-def test_read_weather(test_data):
+def test_read_weather2(test_data):
     response = client.post("/weather/", json=test_data)
     response = client.get(f"/weather/{response.json()['id']}")
     assert response.status_code == 200
     assert response.json()["city"] == test_data["city"]
     assert response.json()["date"] == test_data["date"]
 
-def test_read_non_existent_weather():
+def test_read_non_existent_weather2():
     response = client.get("/weather/2022-01-01")
     assert response.status_code == 404
 
-def test_invalid_date():
+def test_invalid_date2():
     response = client.post("/weather/", json={"city": "london", "day": " invalid date"})
     assert response.status_code == 422
 
